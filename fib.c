148259f50e7004feb6d79c22b1a76f59757e48ac
harrisonthayer <harrisonthayer04@gmail.com>
2024-09-14 14:14:55 -0700

    Fixed recursive function, renamed variables

--- fib.c
+++ fib.c
@@ -34,26 +34,19 @@ int readFileInteger(char *fileName){
 }
 
 /*
- Function fibRecursively takes in 3 
- integers, and calculates the Nth fibonacci
+ Function fibRecursively takes in 1
+ integer, and calculates the Nth fibonacci
  number recursively.
 */
 
-unsigned long long int fibRecursively(int number, int inPrevious, int inCurrent){
-   unsigned long long int counter = number;
-   unsigned long long int b = inPrevious;
-   unsigned long long int c = inCurrent;
-   unsigned long long int a = c;
-   c = c + b;
-   b = a;
-   // Base Case
-   if(counter == 0){
-      return c;
+unsigned long long int fibRecursively(unsigned long long int number){
+   if(number < 1){
+      return 0;
    }
-   // Recursive case
-   c = fibRecursively(counter - 1, b, c);
-
-   return c;
+   if(number == 1){
+      return 1;
+   }
+   return fibRecursively(number-1) + fibRecursively(number-2);
 }
 
 /*
@@ -65,15 +58,15 @@ unsigned long long int fibRecursively(int number, int inPrevious, int inCurrent)
 unsigned long long int fibIteratively(int number){
    // Calculate the actual Fibonacci number,
    // final answer will be stored in "c" when the loop finishes.
-   unsigned long long int a = 0;
-   unsigned long long int b = 1; 
-   unsigned long long int c;
+   unsigned long long int nMinusTwo = 0;
+   unsigned long long int nMinusOne = 1; 
+   unsigned long long int fibonacci;
     for (int i = 0; i < number ; i++) {
-        c = a + b; 
-        a = b;
-        b = c;
+        fibonacci = nMinusTwo + nMinusOne; 
+        nMinusTwo = nMinusOne;
+        nMinusOne = fibonacci;
     }
-   return c;
+   return fibonacci;
 }
 
 
@@ -85,14 +78,14 @@ int main(int argc, char *argv[]) {
    // Read the number in the file, calculate
    // what fib number we will be going to...
    int N = readFileInteger(inputFileName) + inputNumber;
-   int computedFibNumber;
+   unsigned long long int computedFibNumber;
    if(strcmp(rORi,"i") == 0){
       computedFibNumber = fibIteratively(N-2);
-      printf("%i\n", computedFibNumber);
+      printf("%llu\n", computedFibNumber);
    }
    if(strcmp(rORi,"r") == 0){
-      computedFibNumber = fibRecursively(N-3, 0, 1);
-      printf("%i\n", computedFibNumber);
+      computedFibNumber = fibRecursively(N);
+      printf("%llu\n", computedFibNumber);
    }
    return computedFibNumber;
 }
